0a20ecd2db15689e4f28498185d22ae2eee95cce
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-01-10 11:36:54 +0100

    - Added remember me - Cleanup

---
+++ js/bijenkorfcard.custom.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function isAndroid() {
+  return navigator.userAgent.toLowerCase().indexOf("android") > -1;
+}
+
+function isIOS() {
+  return navigator.userAgent.match(/(iPad|iPhone|iPod)/i);
+}
+
+function rememberLogin(loginName, password, keepLoggedIn) {
+  localStorage.setItem("loginName", loginName);
+  localStorage.setItem("password", password);
+  localStorage.setItem("keepLoggedIn", keepLoggedIn);
+}
+
+function isKeepLoggedIn() {
+  return localStorage.getItem("keepLoggedIn") == "true";
+}
+
+function getLoginName() {
+  return localStorage.getItem("loginName");
+}
+
+function getPassword() {
+  return localStorage.getItem("password");
+}
--- js/jqm-init.js
+++ js/jqm-init.js
@@ -6,4 +6,10 @@ $(document).bind("mobileinit", function(){
   $.mobile.pushStateEnabled = false;
   $.mobile.defaultPageTransition = "slide";
   $.event.special.swipe.horizontalDistanceThreshold = 60; // default is 30, but swipe is triggered too often for my taste
+
+  // page loading widget (only required for login)
+  $.mobile.loader.prototype.options.text = "U wordt ingelogd..";
+  $.mobile.loader.prototype.options.textVisible = true;
+  $.mobile.loader.prototype.options.theme = "b";
+//  $.mobile.loader.prototype.options.html = "";
 });
